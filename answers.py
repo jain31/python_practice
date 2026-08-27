10e2b04183689dcfa1d286e2b0958ac93eef2bea
@jain_31 <kashishjain047@gmail.com>
2026-08-27 14:30:32 +0530

    Update answers.py

--- answers.py
+++ answers.py
@@ -1,18 +1,37 @@
 #1.wap to add,sub,multi,divide in a single program 
-a = int(input("Enter first number -> "))
-b = int(input("Enter second number -> "))
-print("choose following for the calculation\n")
-print("1 for sum\n2 for sub\n3 for product\n4 for divide")
-x = int(input("Enter any above options -> "))
-if x == 1:
-    print (a+b)
-elif x==2:
-    print(a-b)
-elif x==3:
-    print(a*b)
-elif x==4:
-    print(a/b)
-else:
-    print("invalid option")
+# a = int(input("Enter first number -> "))
+# b = int(input("Enter second number -> "))
+# print("choose following for the calculation\n")
+# print("1 for sum\n2 for sub\n3 for product\n4 for divide")
+# x = int(input("Enter any above options -> "))
+# if x == 1:
+#     print (a+b)
+# elif x==2:
+#     print(a-b)
+# elif x==3:
+#     print(a*b)
+# elif x==4:
+#     print(a/b)
+# else:
+#     print("invalid option")
 
-print("calculation done")
+# print("calculation done")
+
+#2. dynamic swapping using third variable
+# a = int(input("Enter first number -> "))
+# b = int(input("Enter second number -> "))
+# print ("--before swapping--")
+# print(f"a =",a,"b =",b)
+# c = a
+# a = b
+# b = c
+# print("--after swapping--")
+# print(f"a =",a,"b =",b)
+#3. dynamic swapping wihtout using third variable
+# a = int(input("Enter first number -> "))
+# b = int(input("Enter second number -> "))
+# print ("--before swapping--")
+# print(f"a =",a,"b =",b)
+# a,b = b, a
+# print("--after swapping--")
+# print(f"a =",a,"b =",b)
